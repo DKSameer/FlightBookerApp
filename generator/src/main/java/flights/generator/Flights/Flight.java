5517bc94d9c091fcf12d9b353c808d4513802a09
DKSameer <skyline3600@gmail.com>
2022-10-31 16:12:38 +0100

    Finished up Flight and FDR
    Finished up Flight attributes and methods as well as the Flight Data Randomiser.

--- generator/src/main/java/flights/generator/Flights/Flight.java
+++ generator/src/main/java/flights/generator/Flights/Flight.java
@@ -1,44 +1,167 @@
-import java.time.LocalDate;
-import java.time.LocalTime;
-import org.projectlombok.Data;
+package flights.generator.Flights;
 
-@Data
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class Flight {
 
-    
+    // Input data
+    String origin;
+    String destination;
+    // Simple randomised attributes
+    String airline;
     int flightNumber;
-    LocalDate date;
-    LocalTime time;
     boolean luggageAllowed;
     double price;
-    String airline;
+    // Departure
+    LocalDate departureDate;
+    LocalTime departureTime;
+    LocalDateTime departureDateTime;
+    // Arrival
+    LocalTime arrivalTime;
+    LocalDate arrivalDate;
+    LocalDateTime arrivalDateTime;
+    // Randomised flight duration
+    Duration duration;
+    // Flight Data randomiser
+    FlightDataRandomiser FDR = new FlightDataRandomiser();
 
-    public Flight(LocalDate date){
-        this.number = (int)Math.ceil(Math.random()*900);
-        randomiseDate(date);
-        randomiseTime();
-        this.luggageAllowed = Math.ceil(Math.random())==1;
-        this.price = Math.round(Math.random()*300);
+    public Flight(String inputOrigin, String inputDestination, LocalDate inputDate){
+        this.origin = inputOrigin;
+        this.destination = inputDestination;
+        
+        initialiseDateTimes(inputDate);
+        this.flightNumber = FDR.flightNumber();
+        this.luggageAllowed = FDR.luggageAllowed();
+        this.price = FDR.price();
+        this.airline = FDR.airline();
     }
 
-    private void randomiseDate(LocalDate date){
-        if (Math.ceil(Math.random())==1){
-            date.minusDays((int)Math.ceil(Math.random()*3.99));
-        } else {
-            date.plus((int)Math.ceil(Math.random()*3.99));
-        }
+    private void initialiseDateTimes(LocalDate inputDate){
+        //Flight duration
+        this.duration = FDR.duration();
+        // Initialize randomised flight departure date and time from input
+        this.departureDate = FDR.date(inputDate);
+        this.departureTime = FDR.departureTime();
+        this.departureDateTime = LocalDateTime.of(departureDate,departureTime);
+        // Initialize arrival date and time from departure info and duration
+        this.arrivalDateTime = departureDateTime.plus(duration);
+        this.arrivalDate = arrivalDateTime.toLocalDate();
+        this.arrivalTime = arrivalDateTime.toLocalTime();
     }
 
-    private void randomiseTime(){
-        int hour = Math.floor(randomSeed*24);
-        int minutes = Math.floor(randomSeed*4);
-        this.time = LocalTime().of(hour);
-        switch(minutes){
-            case 0: this.time.plusMinutes(15);
-            case 1: this.time.plusMinutes(15);
-            case 2: this.time.plusMinutes(15);
-            case 3: this.time.plusMinutes(15);
-        }
+    public String getLocation() {
+        return origin;
     }
+
+    public void setLocation(String location) {
+        this.origin = location;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getAirline() {
+        return airline;
+    }
+
+    public void setAirline(String airline) {
+        this.airline = airline;
+    }
+
+    public int getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(int flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public boolean isLuggageAllowed() {
+        return luggageAllowed;
+    }
+
+    public void setLuggageAllowed(boolean luggageAllowed) {
+        this.luggageAllowed = luggageAllowed;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public LocalDate getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(LocalDate departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public LocalTime getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(LocalTime departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public LocalDateTime getDepartureDateTime() {
+        return departureDateTime;
+    }
+
+    public void setDepartureDateTime(LocalDateTime departureDateTime) {
+        this.departureDateTime = departureDateTime;
+    }
+
+    public LocalTime getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(LocalTime arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public LocalDate getArrivalDate() {
+        return arrivalDate;
+    }
+
+    public void setArrivalDate(LocalDate arrivalDate) {
+        this.arrivalDate = arrivalDate;
+    }
+
+    public LocalDateTime getArrivalDateTime() {
+        return arrivalDateTime;
+    }
+
+    public void setArrivalDateTime(LocalDateTime arrivalDateTime) {
+        this.arrivalDateTime = arrivalDateTime;
+    }
+
+    public Duration getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Duration duration) {
+        this.duration = duration;
+    }
+
+    public FlightDataRandomiser getFDR() {
+        return FDR;
+    }
+
+    public void setFDR(FlightDataRandomiser fDR) {
+        FDR = fDR;
+    }
+
 }
-
