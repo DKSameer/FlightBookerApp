5544f6cc5d304870009fbc715703930138c5de34
DKSameer <skyline3600@gmail.com>
2022-11-04 10:55:04 +0100

    Update RestAPITests.java

--- generator/src/test/java/flights/generator/RestAPITests.java
+++ generator/src/test/java/flights/generator/RestAPITests.java
@@ -2,105 +2,100 @@ package flights.generator;
 
 import static io.restassured.RestAssured.*;
 //import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 
 //import org.testng.Assert;
 
 public class RestAPITests {
-	
-	enum Request {
-		  GET,
-		  POST
-		}
-	
-	private static Map<String,Object> input = new HashMap<>();
-	private static Map<String,String> header = new HashMap<>();
-	static String body = "";
-	
-	public static void clearInputMaps() {
-		input.clear();
-		header.clear();
-	}
-	
-	public static void insertInputMap(String where, Object value) {
-		input.put(where, value);
-	}
-	
-	public static void insertHeaderMap(String type, String parameter) {
-		header.put(type,parameter);
-	}
-	
-	//Types: get and post
-	public static JsonPath makeRequest(Map<String, ?> params, String path, Request type) {
-		String body="";
-		Map<String, String> header = new HashMap<>();
-		header.put("content-type", "application/json");
-		return makeRequest(params,path, type, body,header);
-	}
-	
-	public static JsonPath makeRequest(Map<String, ?> params, String path, Request type, String body, Map<String, String> header) {//Add (String body) if you want to receive the body
-		Response temp2 = null;
-		String response = "Error";
-		
-		RequestSpecification temp = given()
-		.headers(header)
-		.queryParams(params)
-		.body(body)
-		.when();
-		
-		switch(type) {
-	      case GET:
-	    	  temp2 = temp.get(path);
-	        break;
-	      case POST:
-	    	  temp2 = temp.post(path);
-	        break;
-	    }
-		
-		response = temp2.then()
-		.assertThat()
-		.statusCode(200).extract().response().asString();
-		
-		return new JsonPath(response);	
-	}
 
-	public static void main(String[] args) {
-		RestAssured.baseURI="localhost:3000/flights";
-		
-		//Get features
-//		System.out.println(makeRequest(input, "/v1/dx/GetFeatures",Request.GET).getList("data").get(0));
-//		//Initialize the session
-//		String sessionId= makeRequest(input, "/v1/dx/InitSession",Request.GET).getString("SessionID");
-//		//Get Defaults
-//		insertInputMap("SessionID", sessionId);
-//		System.out.println(makeRequest(input, "/v1/dx/GetUseDefaultValues",Request.GET).getString("value"));
-//		//Accept the terms
-//		insertInputMap("passphrase", "I have read, understood and I accept and agree to comply with the Terms of Use of EndlessMedicalAPI and Endless Medical services. The Terms of Use are available on endlessmedical.com");
-//		System.out.println(makeRequest(input, "/v1/dx/AcceptTermsOfUse",Request.POST).getString("status"));
-//		clearInputMaps();
-//		//Change Defaults
-//		insertInputMap("SessionID", sessionId);
-//		insertInputMap("value", true);
-//		System.out.println(makeRequest(input, "/v1/dx/SetUseDefaultValues",Request.POST).getString("status"));
-//		clearInputMaps();
-		
-//		RestAssured.baseURI="http://216.10.245.166";
-//		insertHeaderMap("content-type", "application/json");
-//		System.out.println(makeRequest(input, "/Library/Addbook.php",Request.POST,Payload.bodyAddBook(),header).getString(""));
-//		
-		
-//		insertHeaderMap("X-RapidAPI-Key" ,"SIGN-UP-FOR-KEY");
-//		insertHeaderMap("X-RapidAPI-Host" ,"omgvamp-hearthstone-v1.p.rapidapi.com");
-//		String cardname="/cards/%7BYsera%7D";
-//		System.out.println(makeRequest(input, cardname,Request.GET,body,header).getList(""));
+    enum Request {
+        GET,
+        POST
+    }
 
-	}
+    private static Map<String, Object> input = new HashMap<>();
+    private static Map<String, String> header = new HashMap<>();
+    static String body = "";
+
+    public static void clearInputMaps() {
+        input.clear();
+        header.clear();
+    }
+
+    public static Response makeRequest(Map<String, ?> params, String path, Request type, String body,
+            Map<String, String> header) {// Add (String body) if you want to receive the body
+        Response temp2 = null;
+        String response = "Error";
+        header.put("content-type", "application/json");
+        RequestSpecification temp = given()
+                .headers(header)
+                .queryParams(params)
+                .body(body)
+                .when();
+
+        switch (type) {
+            case GET:
+                temp2 = temp.get(path);
+                break;
+            case POST:
+                temp2 = temp.post(path);
+                break;
+        }
+
+        return temp2;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    private static Stream<Arguments> getFlightsArgs() {
+        return Stream.of(
+                Arguments.of("{\"date\" : \"2023-01-10\", \"origin\" : \"Madrid\",\"destination\" : \"Sevilla\"}"),
+                Arguments.of("{\"date\" : \"2023-02-20\", \"origin\" : \"Dublin\",\"destination\" : \"Lisbon\"}"),
+                Arguments.of("{\"date\" : \"2023-03-30\", \"origin\" : \"Rome\",\"destination\" : \"Sao Paulo\"}"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("getFlightsArgs")
+    void testGetFlightsOneway(String bodyString) {
+        RestAssured.baseURI = "http://localhost:8080/destination";
+        body = bodyString;
+        Response response = makeRequest(input, "/day", Request.POST, body, header);
+        assertEquals(201,response.getStatusCode());
+        clearInputMaps();
+    }
+
+    @Test
+    void testGetFlightsRoundtrip() {
+        //
+    }
+
+    @Test
+    void testGetDestination(String bodyString) {
+    }
+
+    @Test
+    void testGetMapping() {
+    }
+
+    @Test
+    void testApplyFilters() {
+    }
 
 }
