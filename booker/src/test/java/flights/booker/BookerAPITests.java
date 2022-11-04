25e839e4eb9e455477d93a8cf1e4c133abf3bee8
DKSameer <skyline3600@gmail.com>
2022-11-04 12:30:04 +0100

    Booking API Controller and tests

--- booker/src/test/java/flights/booker/BookerAPITests.java
+++ booker/src/test/java/flights/booker/BookerAPITests.java
@@ -1,15 +1,24 @@
 package flights.booker;
 
+import static io.restassured.RestAssured.*;
+//import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 
 @SpringBootTest
@@ -24,9 +33,10 @@ public class BookerAPITests {
     private static Map<String, String> header = new HashMap<>();
     static String body = "";
 
-    public static void clearInputMaps() {
+    public static void clearInputProperties() {
         input.clear();
         header.clear();
+        body = "";
     }
 
     public static Response makeRequest(Map<String, ?> params, String path, Request type, String body,
@@ -50,13 +60,75 @@ public class BookerAPITests {
         }
         return temp2;
     }
-    
-    @Test
+
+    private static Stream<Arguments> validFlightArgs() {
+        return Stream.of(
+                // Valid names, card numbers, and expiry dates.
+                Arguments.of("{\"name\" : \"Eric\", \"cardNumber\" : \"4024007198903550\",\"expiryDate\" : \"01/26\"}"),
+                Arguments.of("{\"name\" : \"Dick\", \"cardNumber\" : \"4929514510231509\",\"expiryDate\" : \"02/27\"}"),
+                Arguments.of("{\"name\" : \"Fernando\", \"cardNumber\" : \"5514085136430112\",\"expiryDate\" : \"03/28\"}"));
+    }
+
+    private static Stream<Arguments> invalidFlightArgs() {
+        return Stream.of(
+                // Illegal name
+                Arguments.of("{\"name\" : \"as%$d%f%\", \"cardNumber\" : \"4024007198903550\",\"expiryDate\" : \"01/26\"}"),
+                // Invalid card number
+                Arguments.of("{\"name\" : \"Dick\", \"cardNumber\" : \"402400198550\",\"expiryDate\" : \"01/26\"}"),
+                // Expired card
+                Arguments.of("{\"name\" : \"Fernando\", \"cardNumber\" : \"4532952632815200981\",\"expiryDate\" : \"01/14\"}"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("validFlightArgs")
 	void testConfirmBookingValidRequest(String bodyString) {
-        RestAssured.baseURI = "http://localhost:8080/destination";
+        RestAssured.baseURI = "http://localhost:8081";
         body = bodyString;
-        Response response = makeRequest(input, "/day", Request.POST, body, header);
+        Response response = makeRequest(input, "/book", Request.POST, body, header);
+        clearInputProperties();
         assertEquals(201,response.getStatusCode());
-        clearInputMaps();
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidFlightArgs")
+	void testConfirmBookingInvalidRequest(String bodyString) {
+        RestAssured.baseURI = "http://localhost:8081";
+        body = bodyString;
+        Response response = makeRequest(input, "/book", Request.POST, body, header);
+        clearInputProperties();
+        assertEquals(400,response.getStatusCode());
+    }
+
+    @ParameterizedTest
+    @ValueSource( strings = "{\"name\" : \"as%$d%f%\", \"cardNumber\" : \"4024007198903550\",\"expiryDate\" : \"01/26\"}")
+	void testConfirmBookingInvalidRequestName(String bodyString) {
+        RestAssured.baseURI = "http://localhost:8081";
+        body = bodyString;
+        Response response = makeRequest(input, "/book", Request.POST, body, header);
+        clearInputProperties();
+        String expectedIssue = "Illegal characters in name";
+        assertEquals(expectedIssue, response.getBody().jsonPath().get("message"));
+    }
+
+    @ParameterizedTest
+    @ValueSource( strings = "{\"name\" : \"Eric\", \"cardNumber\" : \"402400\",\"expiryDate\" : \"01/26\"}")
+	void testConfirmBookingInvalidRequestCardNumber(String bodyString) {
+        RestAssured.baseURI = "http://localhost:8081";
+        body = bodyString;
+        Response response = makeRequest(input, "/book", Request.POST, body, header);
+        clearInputProperties();
+        String expectedIssue = "Invalid card number";
+        assertEquals(expectedIssue, response.getBody().jsonPath().get("message"));
+    }
+
+    @ParameterizedTest
+    @ValueSource( strings = "{\"name\" : \"Fernando\", \"cardNumber\" : \"4024007198903550\",\"expiryDate\" : \"01/20\"}")
+	void testConfirmBookingInvalidRequestCardDate(String bodyString) {
+        RestAssured.baseURI = "http://localhost:8081";
+        body = bodyString;
+        Response response = makeRequest(input, "/book", Request.POST, body, header);
+        clearInputProperties();
+        String expectedIssue = "Card has already expired";
+        assertEquals(expectedIssue, response.getBody().jsonPath().get("message"));
     }
 }
